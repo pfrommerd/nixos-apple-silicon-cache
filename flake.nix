eb17d92d8f13dbd02e21969ee57f3c59aa93b340
Daniel Pfrommer <dan.pfrommer@gmail.com>
2026-09-02 02:31:13 -0400

    updated nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,9 @@
 {
   description = "Prebuilt nixos-apple-silicon kernel closure";
 
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.nixos-apple-silicon.url = "github:nix-community/nixos-apple-silicon";
+  inputs.nixos-apple-silicon.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs =
     { nixos-apple-silicon, ... }:
